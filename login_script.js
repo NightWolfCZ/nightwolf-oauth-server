7e61d10ed958b2e9c4661f87a2daa7c6a6b04a64
NightWolfCZ <85906270+NightWolfCZ@users.noreply.github.com>
2025-08-05 00:07:17 +0200

    Update login_script.js

--- login_script.js
+++ login_script.js
@@ -11,7 +11,7 @@ const origins = rawOrigins
   .map(origin => origin.trim())
   .filter(origin => origin.length > 0);
 
-// Regex pro https://domenu.tld i http://localhost
+// Regex: https://doména.cz nebo http://localhost:PORT
 const originRegex = /^https:\/\/([\w-]+\.)+[\w-]+$|^http:\/\/localhost:\d+$/;
 
 const invalidOrigins = origins.filter(origin => !originRegex.test(origin));
@@ -21,29 +21,18 @@ if (invalidOrigins.length > 0) {
   throw new Error(`Invalid origin(s): ${invalidOrigins.join(', ')}`);
 }
 
-
-const REQUIRED_ORIGIN_PATTERN = 
-  /^https:\/\/([\w_-]+\.)+[\w_-]+(,https:\/\/([\w_-]+\.)+[\w_-]+)*$/
-
-console.log('Loaded ORIGINS:', process.env.ORIGINS)
-
-if (
-  typeof process.env.ORIGINS !== 'string' ||
-  !process.env.ORIGINS.match(REQUIRED_ORIGIN_PATTERN)
-) {
-  throw new Error('process.env.ORIGINS MUST be comma separated list of origins that login can succeed on.')
-}
-const origins = process.env.ORIGINS.split(',')
-
-
+// ✅ EXPORTUJEME HTML SE SCRIPTEM
 module.exports = (oauthProvider, message, content) => `
 <script>
 (function() {
   function contains(arr, elem) {
     for (var i = 0; i < arr.length; i++) {
       if (arr[i].indexOf('*') >= 0) {
-        const regex = new RegExp(arr[i].replaceAll('.', '\\\\.').replaceAll('*', '[\\\\w_-]+'))
-        console.log(regex)
+        const regex = new RegExp(
+          arr[i]
+            .replaceAll('.', '\\\\.')
+            .replaceAll('*', '[\\\\w_-]+')
+        );
         if (elem.match(regex) !== null) {
           return true;
         }
@@ -55,21 +44,26 @@ module.exports = (oauthProvider, message, content) => `
     }
     return false;
   }
+
   function recieveMessage(e) {
-    console.log("recieveMessage %o", e)
-    if (!contains(${JSON.stringify(origins)}, e.origin.replace('https://', 'http://').replace('http://', ''))) {
+    console.log("recieveMessage %o", e);
+    const originHost = e.origin
+      .replace('https://', '')
+      .replace('http://', '');
+    if (!contains(${JSON.stringify(origins)}, originHost)) {
       console.log('Invalid origin: %s', e.origin);
       return;
     }
-    // send message to main window with da app
     window.opener.postMessage(
       'authorization:${oauthProvider}:${message}:${JSON.stringify(content)}',
       e.origin
-    )
+    );
   }
-  window.addEventListener("message", recieveMessage, false)
-  // Start handshare with parent
-  console.log("Sending message: %o", "${oauthProvider}")
-  window.opener.postMessage("authorizing:${oauthProvider}", "*")
-})()
-</script>`
+
+  window.addEventListener("message", recieveMessage, false);
+  console.log("Sending message: %o", "${oauthProvider}");
+  window.opener.postMessage("authorizing:${oauthProvider}", "*");
+})();
+</script>
+`;
+
